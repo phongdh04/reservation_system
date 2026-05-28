cf53db35cc9e6bd0daea053f1a60e3ae5f3472de
phongdh04 <phongdh1403@gmail.com>
2026-05-28 22:30:38 +0700

    feat: configure main application with OTP and async features

--- src/main/java/com/example/qlnh/QlnhApplication.java
+++ src/main/java/com/example/qlnh/QlnhApplication.java
@@ -2,12 +2,14 @@ package com.example.qlnh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableRetry
 public class QlnhApplication {
 
     public static void main(String[] args) {
