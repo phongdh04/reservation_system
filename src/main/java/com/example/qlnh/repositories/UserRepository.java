eb67cfc16e8abb797e39e4b9c3913a9d1475e063
phongdh04 <phongdh1403@gmail.com>
2026-05-29 23:56:30 +0700

    feat: add authentication query methods to UserRepository

--- src/main/java/com/example/qlnh/repositories/UserRepository.java
+++ src/main/java/com/example/qlnh/repositories/UserRepository.java
@@ -9,10 +9,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
