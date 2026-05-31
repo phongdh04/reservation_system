33e647979078b290ad841d222613b6dc9273b4e1
phongdh04 <phongdh1403@gmail.com>
2026-05-31 16:50:40 +0700

    refactor: Update TableService

--- src/main/java/com/example/qlnh/services/TableService.java
+++ src/main/java/com/example/qlnh/services/TableService.java
@@ -17,8 +17,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
